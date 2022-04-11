15581ce79f06b4c3c163566fbe13c78f871a3e7a
Oleg Kovalov <oleg@hey.com>
2022-04-11 21:52:08 +0200

    Add TOTP test

---
+++ totp_test.go
@@ -0,0 +1,57 @@
+package otp
+
+import (
+	"encoding/base32"
+	"testing"
+	"time"
+)
+
+type tcTOTP struct {
+	ts     int64
+	code   string
+	mode   Algorithm
+	secret string
+}
+
+var (
+	secretSha1   = base32.StdEncoding.EncodeToString([]byte("12345678901234567890"))
+	secretSha256 = base32.StdEncoding.EncodeToString([]byte("12345678901234567890123456789012"))
+	secretSha512 = base32.StdEncoding.EncodeToString([]byte("1234567890123456789012345678901234567890123456789012345678901234"))
+)
+
+// See: https://datatracker.ietf.org/doc/html/rfc6238#appendix-B
+var totpRFCTestCases = []tcTOTP{
+	{59, "94287082", AlgorithmSHA1, secretSha1},
+	{59, "46119246", AlgorithmSHA256, secretSha256},
+	{59, "90693936", AlgorithmSHA512, secretSha512},
+	{1111111109, "07081804", AlgorithmSHA1, secretSha1},
+	{1111111109, "68084774", AlgorithmSHA256, secretSha256},
+	{1111111109, "25091201", AlgorithmSHA512, secretSha512},
+	{1111111111, "14050471", AlgorithmSHA1, secretSha1},
+	{1111111111, "67062674", AlgorithmSHA256, secretSha256},
+	{1111111111, "99943326", AlgorithmSHA512, secretSha512},
+	{1234567890, "89005924", AlgorithmSHA1, secretSha1},
+	{1234567890, "91819424", AlgorithmSHA256, secretSha256},
+	{1234567890, "93441116", AlgorithmSHA512, secretSha512},
+	{2000000000, "69279037", AlgorithmSHA1, secretSha1},
+	{2000000000, "90698825", AlgorithmSHA256, secretSha256},
+	{2000000000, "38618901", AlgorithmSHA512, secretSha512},
+	{20000000000, "65353130", AlgorithmSHA1, secretSha1},
+	{20000000000, "77737706", AlgorithmSHA256, secretSha256},
+	{20000000000, "47863826", AlgorithmSHA512, secretSha512},
+}
+
+func TestTOTP(t *testing.T) {
+	for _, tc := range totpRFCTestCases {
+		totp, err := NewTOTP(tc.mode, DigitsEight, "cristalhq", 30, 1)
+		failIfErr(t, err)
+
+		at := time.Unix(tc.ts, 0).UTC()
+		code, err := totp.GenerateCode(tc.secret, at)
+		failIfErr(t, err)
+		mustEqual(t, code, tc.code)
+
+		err = totp.Validate(tc.code, at, tc.secret)
+		failIfErr(t, err)
+	}
+}
